8d186fe25f0037d4597ec0e646e07ed62241d85e
itegration0 <itegration.mail@gmail.com>
2026-02-12 13:30:03 +0300

    Update timeout.component.ts pdf file

--- src/app/components/timeout.component.ts
+++ src/app/components/timeout.component.ts
@@ -44,7 +44,7 @@ import { CommonModule } from '@angular/common';
 export class TimeoutComponent {
   downloadPdf(): void {
     const a = document.createElement('a');
-    a.href = '/assets/maintenance-guide.pdf';
+    a.href = '/assets/Без WA.pdff';
     a.download = 'Памятка по обслуживанию.pdf';
     document.body.appendChild(a);
     a.click();
